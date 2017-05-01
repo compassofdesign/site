611e1691b29e0f1e58b20f0431e39f34c4659b07
darian <git@darianrosebrook.com>
2017-05-01 06:10:51 -0700

    started setting the scripts for logic with event listeners on the questions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,11 @@
+
+var total, section1, s1answers, section2, s2answers, section3, s3answers, section4, s4answers, questions, submit;
+s1answers = [];
+s2answers = [];
+s3answers = [];
+s4answers = [];
+
+
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
@@ -10,5 +18,13 @@ $(document).ready(function(){
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
+	});
+	submit = $('#submit');
+	submit.on('click', function(){
+		alert('hi');
+	})
+	questions = $('[id^="q"]:not([id*="-"])');
+	questions.on('click', function(e){
+
 	});
 });
